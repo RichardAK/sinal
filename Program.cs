eee7fdc8f8e09637685a5d5151be761aaa4841c3
Kauã <kau.lira@yahoo.com>
2022-04-01 21:57:22 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int número;
+
+Console.Write("Digite um número: ");
+número = Convert.ToInt32(Console.ReadLine());
+
+if (número < 0)
+{
+    Console.ForegroundColor = ConsoleColor.DarkBlue;           
+    Console.WriteLine("Negativo");
+}
+
+else if (número > 0)
+{
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("Positivo");
+}
+
+else
+{
+    Console.ForegroundColor = ConsoleColor.White; 
+    Console.WriteLine("zero");
+}
+
+Console.ResetColor(); 
